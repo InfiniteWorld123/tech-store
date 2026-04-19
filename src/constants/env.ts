c5f83e0043c7f0771e746bb067369555cb8809c2
‪Yaman Warda‬‏ <Wardayaman47@gmail.com>
2026-04-19 21:43:50 +0200

    my changes

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -24,14 +24,14 @@ const getBooleanEnvVar = (key: string, fallback: boolean) => {
 
 export const env = {
 	BASE_URL: getEnvVar("BASE_URL"),
-	API_KEY: getEnvVar("BETTER_AUTH_SECRET"),
+	// API_KEY: getEnvVar("BETTER_AUTH_SECRET"),
 	DATABASE_URL: getEnvVar("DATABASE_URL"),
-	RESEND: getEnvVar("RESEND"),
-	EMAIL_FROM:
-		getOptionalEnvVar("EMAIL_FROM") ?? "StoryNest <noreply@storynest.app>",
-	CONTACT_TO_EMAIL: getOptionalEnvVar("CONTACT_TO_EMAIL"),
-	UPLOADTHING_TOKEN: getOptionalEnvVar("UPLOADTHING_TOKEN"),
-	ENABLE_SEED_ROUTES: getBooleanEnvVar("ENABLE_SEED_ROUTES", false),
+	// RESEND: getEnvVar("RESEND"),
+	// EMAIL_FROM:
+	// 	getOptionalEnvVar("EMAIL_FROM") ?? "StoryNest <noreply@storynest.app>",
+	// CONTACT_TO_EMAIL: getOptionalEnvVar("CONTACT_TO_EMAIL"),
+	// UPLOADTHING_TOKEN: getOptionalEnvVar("UPLOADTHING_TOKEN"),
+	// ENABLE_SEED_ROUTES: getBooleanEnvVar("ENABLE_SEED_ROUTES", false),
 } as const;
 
 export type EnvVariables = typeof env;
